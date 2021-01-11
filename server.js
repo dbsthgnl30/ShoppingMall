c04bf155f4e9af0497ec9b06d7a98c1c6fdb641f
JeongHyun <morningsky71@gmail.com>
2021-01-11 22:37:47 +0900

    2021.01.11 DB 테이블 구축 및  EXPRESS 와 연동하기

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express =require('express');
-
+const fs=require('fs');
 const bodyParser =require('body-parser');
 const app=express();
 const port=process.env.PORT || 5000;
@@ -7,9 +7,60 @@ const port=process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.get('/api/hello',(req, res) => {
-    res.send({message : 'hello Express!'})
+//데이타베이스 정보 조히
+const data=fs.readFileSync('./database.json');
+const conf=JSON.parse(data);
+
+//mysql 열결설정
+const mysql= require('mysql');
+const connection =mysql.createConnection({
+  host:conf.host,
+  user: conf.user,
+  password: conf.password,
+  port : conf.port,
+  database : conf.database
+
+});  
+
+//mysql연결
+connection.connect();
+
+
+// API를 이용한 SQL조회
+app.get('/api/customers',(req,res) => {
+
+    connection.query(
+      "SELECT * FROM CUSTOMER",
+      (err,rows,fields) =>{
+        res.send(rows);   // 값 리턴
+      }
+ 
+    );
+
+// API를 이용한 SQL조회
+app.get('/api/goodList',(req,res) => {
+
+  connection.query(
+    "SELECT * FROM CUSTOMER",
+    (err,rows,fields) =>{
+      res.send(rows);   // 값 리턴
+    }
+  );
+
+
+  // API를 이용한 SQL조회
+app.get('/api/goodList',(req,res) => {
+
+  connection.query(
+    "SELECT * FROM CUSTOMER",
+    (err,rows,fields) =>{
+      res.send(rows);   // 값 리턴
+    }
+  );
+
+
 });
 
-app.listen(port,()=> console.log('Listening on port ${port}'));
+
+app.listen(port,()=> console.log('Listening on port 5000'));
 
